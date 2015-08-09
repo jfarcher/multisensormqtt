410867b0153e5d049f11e92f8e685cc9fc65ca4e
Jon Archer <jf_archer@yahoo.com>
2015-08-10 00:06:27 +0100

    Added new BMP180 sensor into the mix, publishes Temp, Pressure and Altitude to independant topics

--- multisensormqtt.ino
+++ multisensormqtt.ino
@@ -5,6 +5,7 @@
 #include "TSL2561.h"
 #include "DHT.h"
 #include "config.h"
+#include <Adafruit_BMP085.h>
 
 //Set Pins
 int DHTPIN = 6;
@@ -17,10 +18,27 @@ TSL2561 tsl(TSL2561_ADDR_FLOAT);
 // Update these with values suitable for your network.
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 char charSensorVal[20];  
-
+Adafruit_BMP085 bmp;
 
 EthernetClient ethClient;
 PubSubClient client(MQTT_SERVER, 1883, 0, ethClient);
+void getBMP()
+{
+ char bmpt_buffer[100];
+ char bmpp_buffer[100];
+ char bmpa_buffer[100];
+ float bmptf = bmp.readTemperature(); 
+ char* bmpt = dtostrf(bmptf,4,2,bmpt_buffer);
+ float bmppf = bmp.readPressure(); 
+ char* bmpp = dtostrf(bmppf,4,2,bmpp_buffer);
+ float bmpaf = bmp.readAltitude();
+ char* bmpa = dtostrf(bmpaf,4,2,bmpa_buffer);
+ client.publish(MQTT_BMPT_TOPIC, bmpt);
+ client.publish(MQTT_BMPP_TOPIC, bmpp);
+ client.publish(MQTT_BMPA_TOPIC, bmpa);
+ delay(5000);
+}
+
 void getLum()
 {
 
@@ -51,18 +69,24 @@ client.publish(MQTT_TEMP_TOPIC, temp);
 client.publish(MQTT_HUM_TOPIC, hum);
 }
 void setup() {
+
 attachInterrupt(0,pir,HIGH);
+Serial.begin(9600);
 
   Ethernet.begin(mac);
   byte ip = Ethernet.localIP();
+  
   if(client.connect(CLIENT_ID)){
   client.publish(MQTT_ROOM_TOPIC,"PIR Sensor UP");
   client.publish(MQTT_ROOM_TOPIC,"DHT Sensor Up");
   }
   if (tsl.begin()){
     client.publish(MQTT_ROOM_TOPIC,"LUM Sensor UP");
+    
   }
-   
+    if (bmp.begin()){ 
+      client.publish(MQTT_ROOM_TOPIC,"BMP Sensor UP");
+      } 
   dht.begin();
       
    
@@ -85,6 +109,7 @@ void loop() {
 
      getDHT();
      getLum();
+     getBMP();
   client.loop();
 delay(500);
 }
